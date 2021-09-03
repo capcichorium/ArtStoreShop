6180b0de6f4c96a2690f2a6a948272b28d1c545b
Vlad Gimadiev <vlad.gimadiev@mail.ru>
2021-09-03 17:59:53 +0300

    Создание класса Product

---
+++ ArtStoreShop/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ArtStoreShop.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string desc { get; set; }
+        public double price { get; set; }
+        public bool available { get; set; }
+        public int categoryId { get; set; }
+        public bool isFavorite { get; set; }
+        public virtual Category category { get; set; }
+    }
+}

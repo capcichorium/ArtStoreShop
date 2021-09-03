96e0ad26d9b38dfab52e0d46c8166ebcc56d34e5
Vlad Gimadiev <vlad.gimadiev@mail.ru>
2021-09-03 17:57:33 +0300

    Создание класса User

---
+++ ArtStoreShop/Models/User.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ArtStoreShop.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string adress { get; set; }
+    }
+}

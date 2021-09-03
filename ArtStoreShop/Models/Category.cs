c0767d811fc65abba31d0f51bbc96addbf051da3
Vlad Gimadiev <vlad.gimadiev@mail.ru>
2021-09-03 17:58:46 +0300

    Создание класса Category

---
+++ ArtStoreShop/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ArtStoreShop.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int desc { get; set; }
+    }
+}
